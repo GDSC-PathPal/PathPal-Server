c068ed242349e6781edd453432e26063f7d993dd
seungtaekhong <hsk4991149@naver.com>
2023-12-26 13:33:10 +0900

    feat: test method

---
+++ src/main/java/solution/gdsc/PathPal/Dto.java
@@ -0,0 +1,10 @@
+package solution.gdsc.PathPal;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Dto {
+    private String name;
+}
--- src/main/java/solution/gdsc/PathPal/MainController.java
+++ src/main/java/solution/gdsc/PathPal/MainController.java
@@ -1,6 +1,7 @@
 package solution.gdsc.PathPal;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class MainController {
     @GetMapping
     public String index() {
         return "hello world!";
+    }
+
+    @GetMapping
+    public String test(@RequestBody Dto dto) {
+        return "받은 데이터 = " + dto.getName();
     }
 }
