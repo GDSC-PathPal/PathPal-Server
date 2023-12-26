d2eb9f05c6c2c2cdb0d16dfce7fcd2b1539df91d
seungtaekhong <hsk4991149@naver.com>
2023-12-26 11:42:27 +0900

    initial

---
+++ src/main/java/solution/gdsc/PathPal/PathPalApplication.java
@@ -0,0 +1,13 @@
+package solution.gdsc.PathPal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathPalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PathPalApplication.class, args);
+	}
+
+}
