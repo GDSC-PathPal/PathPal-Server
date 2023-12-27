9b6c8c09c4405fdcb795cba32517e7c97d3aae4c
seungtaekhong <hsk4991149@naver.com>
2023-12-27 16:13:28 +0900

    feat: Socket image 예시 생성

--- src/main/java/solution/gdsc/PathPal/MainController.java
+++ src/main/java/solution/gdsc/PathPal/MainController.java
@@ -1,12 +1,22 @@
 package solution.gdsc.PathPal;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @RestController
 public class MainController {
+
+    @Value("${image.path}")
+    private String path;
 
     @GetMapping
     public String index() {
@@ -16,5 +26,17 @@ public class MainController {
     @PostMapping("/test")
     public TestResponse test(@RequestBody TestRequest testRequest) {
         return TestResponse.from(testRequest.getName());
+    }
+
+    @GetMapping("/image/{imageId}")
+    public ResponseEntity<Resource> viewImg(@PathVariable(name = "imageId") Integer imageId) throws IOException {
+        final String fileFullPath = path + imageId + ".jpeg";
+        System.out.println(fileFullPath);
+        Path path = new File(fileFullPath).toPath();
+
+        FileSystemResource resource = new FileSystemResource(path);
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(Files.probeContentType(path)))
+                .body(resource);
     }
 }
---
+++ src/main/java/solution/gdsc/PathPal/global/config/WebSocketHandler.java
@@ -0,0 +1,63 @@
+package solution.gdsc.PathPal.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.BinaryMessage;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.BinaryWebSocketHandler;
+
+import javax.imageio.stream.FileImageOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Component
+public class WebSocketHandler extends BinaryWebSocketHandler {
+
+    @Value("${image.path}")
+    private String path;
+
+    private final Set<WebSocketSession> sessions = ConcurrentHashMap.newKeySet();
+
+    private final AtomicInteger imageNumber = new AtomicInteger(1);
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        session.setBinaryMessageSizeLimit(1024 * 1024 * 10);
+        sessions.add(session);
+    }
+
+    @Override
+    protected void handleBinaryMessage(WebSocketSession session, BinaryMessage message) throws Exception {
+        byte[] bytes = message.getPayload().array();
+
+        String responseMessage = "";
+        try {
+            int imageId = imageNumber.get();
+            String fileFullName = path + imageId + ".jpeg";
+            System.out.println(fileFullName);
+
+            FileImageOutputStream imageOutput = new FileImageOutputStream(new File(fileFullName));
+            imageOutput.write(bytes, 0, bytes.length);
+            imageOutput.close();
+            responseMessage = "성공적으로 저장했습니다. 받은 바이트 크기=" + bytes.length + ", savedImageId=" + imageId;
+            imageNumber.incrementAndGet();
+        } catch (Exception e) {
+            responseMessage = "저장에 실패했습니다. 받은 바이트 크기=" + bytes.length;
+        }
+        try {
+            session.sendMessage(new TextMessage(responseMessage));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        sessions.remove(session);
+    }
+}
